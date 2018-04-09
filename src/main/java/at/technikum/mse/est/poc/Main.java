e30a6146ecb4b3d0262251aba570fb833af8af04
flpa <flpa.dev@zoho.com>
2018-04-09 21:00:11 +0200

    add sl4fj + simple impl for testing

--- src/main/java/at/technikum/mse/est/poc/Main.java
+++ src/main/java/at/technikum/mse/est/poc/Main.java
@@ -1,14 +1,19 @@
 package at.technikum.mse.est.poc;
 
 import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dtos.Student;
 
 public class Main {
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) throws Exception {
+		LOG.info("Starting");
+		
 		Reader reader = new ExcelReader();
 		Workbook wb = reader.createWorkbook("resources/Students.xlsx");
 		reader.mapSheetToClass(wb, 0, Student.class);
-		
 	}
 }
