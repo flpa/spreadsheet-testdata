d9e553beea9cc0456d741e46f0b725a81b512bcb
flpa <flpa.dev@zoho.com>
2018-04-22 12:34:52 +0200

    fix broken build

--- src/main/java/at/technikum/mse/est/examples/GenerateTemplateWithDefaultFileMapper.java
+++ src/main/java/at/technikum/mse/est/examples/GenerateTemplateWithDefaultFileMapper.java
@@ -1,12 +1,13 @@
 package at.technikum.mse.est.examples;
 
+import at.technikum.mse.est.EstException;
 import at.technikum.mse.est.LibraryApi;
 import at.technikum.mse.est.poc.TestClass;
 
 import java.io.File;
 
 public class GenerateTemplateWithDefaultFileMapper {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EstException {
         String filename = "example.xlsx";
         LibraryApi libraryApi = new LibraryApi();
         libraryApi.createTemplate(new File(filename), TestClass.class);
